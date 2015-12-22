daa1aea9c34a97ef6c31c2377a417c8d637f80e3
Ron Sparks <ron.sparks@gmail.com>
2015-12-22 18:33:11 -0500

    (part 2) working on getting the RecylcerView.Adapter to update and refresh the view when I get new data.

--- app/src/main/java/com/ronsparks/popularmovies/fragments/MovieMasonryFragment.java
+++ app/src/main/java/com/ronsparks/popularmovies/fragments/MovieMasonryFragment.java
@@ -7,7 +7,6 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -63,9 +62,6 @@ public class MovieMasonryFragment extends Fragment {
 
         MovieOperations movieOps = new MovieOperations();
         String discoverUrl = movieOps.buildDiscoverMoviesUrl(getContext(), null);
-        //
-        // TODO: WHY IS CONTEXT NULL HERE?
-        //
         new AsyncMovieFragmentRunner(mContext).execute(discoverUrl);
 
         setHasOptionsMenu(true);
